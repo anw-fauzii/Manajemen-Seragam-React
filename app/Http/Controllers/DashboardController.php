f1807fa289789c65d5687198acdac70f1069d34f
anw-fauzii <anwar.fauzy18@gmail.com>
2024-05-09 10:31:34 +0700

    Role and Permissions

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,6 +7,7 @@ use App\Models\SeragamDetail;
 use App\Models\Supplier;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
 
@@ -14,55 +15,61 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        $bulan_pesanan = Pesanan::where('status', false)
-            ->select(DB::raw('YEAR(created_at) as year'), DB::raw('MONTH(created_at) as month'), DB::raw('COUNT(*) as total'))
-            ->groupBy(DB::raw('YEAR(created_at)'), DB::raw('MONTH(created_at)'))
-            ->get();
+        $user = User::find(Auth::user()->id);
+        if ($user->hasRole('admin')) {
 
-        // Initialize an array to hold the formatted data
-        $seriesData = [];
+            $bulan_pesanan = Pesanan::where('status', false)
+                ->select(DB::raw('YEAR(created_at) as year'), DB::raw('MONTH(created_at) as month'), DB::raw('COUNT(*) as total'))
+                ->groupBy(DB::raw('YEAR(created_at)'), DB::raw('MONTH(created_at)'))
+                ->get();
 
-        // Loop through each item in $jumlah_pesanan and format it for Highcharts
-        foreach ($bulan_pesanan as $item) {
-            // Define month names (optional, you can also use month numbers directly)
-            $monthNames = [
-                1 => 'January',
-                2 => 'February',
-                3 => 'March',
-                4 => 'April',
-                5 => 'May',
-                6 => 'June',
-                7 => 'July',
-                8 => 'August',
-                9 => 'September',
-                10 => 'October',
-                11 => 'November',
-                12 => 'December',
-            ];
+            // Initialize an array to hold the formatted data
+            $seriesData = [];
 
-            // Format the data into an associative array with 'name' and 'data' keys
-            $seriesData[$item->year][] = [
-                'name' => $monthNames[$item->month], // Month name
-                'data' => [(int) $item->total] // Total orders for the month
-            ];
+            // Loop through each item in $jumlah_pesanan and format it for Highcharts
+            foreach ($bulan_pesanan as $item) {
+                // Define month names (optional, you can also use month numbers directly)
+                $monthNames = [
+                    1 => 'Januari',
+                    2 => 'Februari',
+                    3 => 'Maret',
+                    4 => 'April',
+                    5 => 'Mei',
+                    6 => 'Juni',
+                    7 => 'Juli',
+                    8 => 'Agustus',
+                    9 => 'September',
+                    10 => 'Oktober',
+                    11 => 'November',
+                    12 => 'Desember',
+                ];
+
+                // Format the data into an associative array with 'name' and 'data' keys
+                $seriesData[$item->year][] = [
+                    'name' => $monthNames[$item->month], // Month name
+                    'data' => [(int) $item->total] // Total orders for the month
+                ];
+            }
+
+            $jumlah_pesanan = Pesanan::where('status', FALSE)->count();
+            $pesanan = Pesanan::with(
+                'pesanan_detail',
+                'pesanan_detail.seragam_detail',
+                'pesanan_detail.seragam_detail.seragam',
+            )->latest()->take(10)->get();
+            $supplier = Supplier::all()->count();
+            $stok_seragam = SeragamDetail::sum('stok');
+            $user = User::all()->count();
+            return Inertia::render('Dashboard', [
+                'jumlah_pesanan' => $jumlah_pesanan,
+                'supplier' => $supplier,
+                'stok_seragam' => $stok_seragam,
+                'user' => $user,
+                'pesanan' => $pesanan,
+                'seriesData' => $seriesData
+            ]);
+        } else {
+            return Inertia::render('Error/404');
         }
-
-        $jumlah_pesanan = Pesanan::where('status', FALSE)->count();
-        $pesanan = Pesanan::with(
-            'pesanan_detail',
-            'pesanan_detail.seragam_detail',
-            'pesanan_detail.seragam_detail.seragam',
-        )->latest()->take(2)->get();
-        $supplier = Supplier::all()->count();
-        $stok_seragam = SeragamDetail::sum('stok');
-        $user = User::all()->count();
-        return Inertia::render('Dashboard', [
-            'jumlah_pesanan' => $jumlah_pesanan,
-            'supplier' => $supplier,
-            'stok_seragam' => $stok_seragam,
-            'user' => $user,
-            'pesanan' => $pesanan,
-            'seriesData' => $seriesData
-        ]);
     }
 }
