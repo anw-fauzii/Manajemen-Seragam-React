884485f2780434fa014a589056d4108f82499c0f
anw-fauzii <anwar.fauzy18@gmail.com>
2024-05-12 18:43:35 +0700

    error view

--- resources/js/Pages/Error/403.jsx
+++ resources/js/Pages/Error/403.jsx
@@ -1,10 +1,10 @@
 import { Link } from "@inertiajs/react"
-export default function notFound() {
+export default function Forbidden() {
     return (
         <section className="bg-white dark:bg-gray-900 min-h-screen flex items-center">
             <div className="container mx-auto px-4">
                 <div className="max-w-screen-md mx-auto text-center">
-                    <h1 className="text-6xl md:text-8xl font-extrabold text-primary-600 dark:text-primary-500">404</h1>
+                    <h1 className="text-6xl md:text-8xl font-extrabold text-primary-600 dark:text-primary-500">403</h1>
                     <p className="text-2xl md:text-3xl font-bold text-gray-900 dark:text-white mb-4">Oops! Something's missing.</p>
                     <p className="text-lg md:text-xl font-light text-gray-500 dark:text-gray-400 mb-8">Sorry, we couldn't find the page you're looking for.</p>
                     <Link replace href={route('welcome')}>
