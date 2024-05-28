c3e57b0b3ca2cb4545944d22bfe2a8a2e55acdab
anw-fauzii <anwar.fauzy18@gmail.com>
2024-05-28 23:10:08 +0700

    Pencarian

--- resources/js/Pages/Frontend/Welcome.jsx
+++ resources/js/Pages/Frontend/Welcome.jsx
@@ -1,20 +1,13 @@
-import { Link, Head, useForm, router } from '@inertiajs/react';
+import { Link, Head } from '@inertiajs/react';
 import Bottom from '@/Layouts/Frontend/Bottom';
 import Navbar from '@/Layouts/Frontend/Navbar';
 import Judul from '@/Components/Judul';
 import React from 'react';
 import { Carousel } from 'flowbite-react';
 import { NumericFormat } from 'react-number-format';
-import { useState } from 'react';
+import Pencarian from './Pencarian';
 
 export default function Welcome(props) {
-    const query = props.query || '';
-    const [searchQuery, setSearchQuery] = useState(query);
-
-    const handleSearch = (e) => {
-        e.preventDefault();
-        router.get('/data-seragam/cari', { q: searchQuery });
-    };
 
     return (
         <div className='top-0 left-0 min-h-screen bg-gray-100 bg-cover'>
@@ -27,19 +20,7 @@ export default function Welcome(props) {
                 <Judul gambar="/storage/SD.jpg" kategori="3" />
             </div>
             <div className="h-64 sm:h-64 xl:h-80 2xl:h-96 mt-16 p-4 block lg:hidden bg-gray-100 bg-cover">
-                <form onSubmit={handleSearch} className="max-w-md mx-auto mb-4">
-                    <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Cari</label>
-                    <div className="relative">
-                        <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                            <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                            </svg>
-                        </div>
-                        <input value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} type="search" id="default-search" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Cari seragam ..." required />
-                        <button type="submit" className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Cari</button>
-                    </div>
-                </form>
-
+                <Pencarian />
                 <Carousel className='h-full mb-4'>
                     <img src="/storage/PG.jpg" className='w-full h-full object-cover' alt="PG" />
                     <img src="/storage/TK.jpg" className='w-full h-full object-cover' alt="TK" />
